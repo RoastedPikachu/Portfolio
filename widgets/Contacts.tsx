09427146265b8768335438cc053182936ef839ce
boris <b.karabyt@gmail.com>
2025-08-31 15:25:07 +0300

    feat: Added contacts section

---
+++ widgets/Contacts.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "@/styles/widgets/contacts.css";
+
+const Contacts = () => {
+  return (
+    <section className="contacts">
+      <h3 className="contacts-title">Want to hire me?</h3>
+
+      <button className="contacts-button">Contact me</button>
+    </section>
+  );
+};
+
+export default Contacts;
