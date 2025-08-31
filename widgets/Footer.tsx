4254fb43a31429aee2def182889dc9c7f4216ca5
boris <b.karabyt@gmail.com>
2025-08-31 15:08:20 +0300

    feat: Added footer

---
+++ widgets/Footer.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useState } from "react";
+
+import "@/styles/footer.css";
+
+const Footer = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const scrollToTop = () => {
+    document.body.scrollTop = 0;
+  };
+
+  return (
+    <footer className="footer">
+      <span className="footer-textBlock">
+        <p className="footer-textBlock-copyright">&copy;</p>{" "}
+        {currentDate.getFullYear()} / Boris Karabut / ALL RIGHTS RESERVED
+      </span>
+
+      <button onClick={scrollToTop} className="footer-scrollButton">
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 22 22"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.061 0 2.72 8.342a1.77 1.77 0 0 0 0 2.506 1.77 1.77 0 0 0 2.506 0l4.063-4.064v13.444a1.772 1.772 0 1 0 3.545 0V6.784l4.063 4.064a1.77 1.77 0 0 0 2.506-.001 1.77 1.77 0 0 0 0-2.505z"
+            fill="#e4e9ff"
+          />
+        </svg>
+
+        <p>Scroll to top</p>
+      </button>
+    </footer>
+  );
+};
+
+export default Footer;
