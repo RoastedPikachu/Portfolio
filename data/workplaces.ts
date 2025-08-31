9ab169577e46047f7e1cd1039587525f66305ab9
boris <b.karabyt@gmail.com>
2025-08-31 21:16:46 +0300

    feat: Added work experience section

---
+++ data/workplaces.ts
@@ -0,0 +1,72 @@
+export const workplaces = [
+  {
+    id: 1,
+    logo: "/static/coordinateSystem.png",
+    occupation: "Middle Frontend developer",
+    companyName: "Coordinate System LLC",
+    timeGap: "Jul 2024 - Jun 2025",
+    results: [
+      {
+        id: 1,
+        text: "Launched the Startup Cycle project, which got into the Skolkovo startup pre-accelerator.",
+      },
+      {
+        id: 2,
+        text: "Developed the service from scratch: I designed the architecture, I chose the technology stack.",
+      },
+      {
+        id: 3,
+        text: "Optimized application performance: reduced loading time, improved interface responsiveness.",
+      },
+      {
+        id: 4,
+        text: "Covered the code with unit tests (Jest, React Testing Library), increasing the stability of the product.",
+      },
+      {
+        id: 5,
+        text: "Participated in the design of new functionality: offered ideas, discussed UX / UI with the team.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    logo: "/static/ahiss.png",
+    occupation: "Junior Frontend developer",
+    companyName: "AHISS",
+    timeGap: "May 2023 - Apr 2024",
+    results: [
+      {
+        id: 1,
+        text: "Launched a website of the most popular cybersecurity competition in the Russian Federation (CTF Cup)",
+      },
+      {
+        id: 2,
+        text: "Launched a website for the sale of merch for participants of CTF competitions and events.",
+      },
+      {
+        id: 3,
+        text: "Developed a SPA (Full cycle: from idea to deployment).",
+      },
+      {
+        id: 4,
+        text: "Migrated one of the projects from Vue to React.",
+      },
+      {
+        id: 5,
+        text: "Optimized existing projects.",
+      },
+      {
+        id: 6,
+        text: "Wrote unit tests.",
+      },
+      {
+        id: 7,
+        text: "Estimated task deadlines.",
+      },
+      {
+        id: 8,
+        text: "Close interacted with all team members (from the designer to the information security specialist).",
+      },
+    ],
+  },
+];
